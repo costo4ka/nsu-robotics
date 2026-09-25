97e9d92a862d5226ebaa4575c510b019d7206d83
costo4ka <a.tebaikina@g.nsu.ru>
2026-09-25 18:30:21 +0700

    Implement PR02 turtle bringup

--- README.md
+++ README.md
@@ -52,3 +52,22 @@ Expected result:
 - domain 16 / 16: communication works
 - domain 16 / 17: communication is lost
 - domain 16 / 16 again: communication is restored
+
+## PR02 — turtle_bringup
+
+Создан ROS 2 пакет `turtle_bringup` с launch-файлом для запуска `turtlesim`.
+
+Сборка:
+
+`source /opt/ros/jazzy/setup.bash`
+`colcon build --symlink-install --packages-select turtle_bringup`
+`source install/setup.bash`
+
+Запуск:
+
+`export ROS_DOMAIN_ID=16`
+`ros2 launch turtle_bringup sim.launch.py`
+
+Для управления использовался топик `/turtle1/cmd_vel` типа `geometry_msgs/msg/Twist`.
+При публикации того же сообщения в `/cmd_vel` publisher существовал, но подписчиков не было.
+После исправления имени на `/turtle1/cmd_vel` движение восстановилось.
---
+++ src/turtle_bringup/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'turtle_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='arina',
+    maintainer_email='a.tebaikina@g.nsu.ru',
+    description='Launch package for turtlesim',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
